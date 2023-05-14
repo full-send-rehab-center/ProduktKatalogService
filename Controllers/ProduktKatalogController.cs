6c447a3b68471cc4c51db1771497d0a9249f2c13
Wxlfgang <oellegaardwilliam@gmail.com>
2023-05-14 21:59:39 +0200

    first commit

---
+++ Controllers/ProduktKatalogController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProduktKatalogService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ProduktKatalogController : ControllerBase
+{
+    
+}
---
+++ Models/ProduktKatalog.cs
@@ -0,0 +1,6 @@
+namespace ProduktKatalogService;
+
+public class ProduktKatalog
+{
+
+}
