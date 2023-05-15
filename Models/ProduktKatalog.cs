6cfb4bb2c445484963e85402b713e733f6af9125
wxlfgang <oellegaardwilliam@gmail.com>
2023-05-15 10:44:15 +0200

    hotfix

--- Models/ProduktKatalog.cs
+++ Models/ProduktKatalog.cs
@@ -2,5 +2,9 @@ namespace ProduktKatalogService;
 
 public class ProduktKatalog
 {
+public string CategoryCode{get; set;}
+public string CategoryName{get; set;}
+public string ItemDescription{get; set;}
+public DateTime Auctiondate{get;set;}
 
 }
